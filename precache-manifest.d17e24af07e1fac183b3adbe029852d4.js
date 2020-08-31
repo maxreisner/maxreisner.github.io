9b2a29fd533a9e341fed54397dabcac1cd94a595
maxreisner <maximusreisner@gmail.com>
2020-08-31 00:20:31 -0400

    Updates

---
+++ precache-manifest.d17e24af07e1fac183b3adbe029852d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fd2063851764149622b97fd6582426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "567f91de3b1fe2f7c635",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9bf9bc888987aabc09ae",
+    "url": "/static/js/2.d19ac9c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d19ac9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567f91de3b1fe2f7c635",
+    "url": "/static/js/main.8a4bedba.chunk.js"
+  },
+  {
+    "revision": "028a0bd770e88702b5cf",
+    "url": "/static/js/runtime-main.e3975ec1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
